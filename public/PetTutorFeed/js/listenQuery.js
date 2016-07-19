c2802f986c81caebb769f5c998018097b9296d7d
Shae Judge <shaejudge@hotmail.com>
2016-07-19 13:41:55 -0400

    Fix for https 8

--- public/PetTutorFeed/js/listenQuery.js
+++ public/PetTutorFeed/js/listenQuery.js
@@ -4,7 +4,7 @@ Parse.$ = jQuery;
 Initialize Parse with your Parse application javascript keys
 ~~~~~~~~~~~~~~~~~~~~*/
 Parse.initialize("123123123");
-Parse.serverURL = "http://localhost:1337/parse"
+Parse.serverURL = "https://ptremotetest.herokuapp.com/parse"
 
 var RemoteFeed = Parse.Object.extend("RemoteFeed");
 var parObj;
