9e753d662e41179202a286aa2ebde160195418fe
DavidDuffek <david@duffekmobile.com>
2016-07-27 13:50:00 -0400

    test poll

--- cloud/main.js
+++ cloud/main.js
@@ -9,38 +9,41 @@ Parse.Cloud.define('hello', function(req, res) {
 Parse.Cloud.define('checkFeedStatus', function(request, response) {
 
     var startTime = new Date().getTime(); // Get the start time
-    var timeLimit = 10000; // max time 10 seconds
+    var timeLimit = 8000; // max time 10 seconds
     
 
     // Function we want to repeat
-    function checkForChange() {
-        
-        var q = new Parse.Query( Parse.Object.extend('RemoteFeed') );
-        q.get('OhHL5g0ToP').then(function(remoteFeedObject) {
+    // function checkForChange() {
+    //
+    //     var q = new Parse.Query( Parse.Object.extend('RemoteFeed') );
+    //     q.get('OhHL5g0ToP').then(function(remoteFeedObject) {
+    //
+    //         var currentTime = new Date().getTime();
+    //
+    //         // if feed var was set, respond with true
+    //         if(remoteFeedObject.get("shouldFeed")) {
+    //             response.success("Should feed");
+    //         }
+    //         // if we havent reached the time limit, check again
+    //         else if (currentTime - startTime < timeLimit) {
+    //
+    //             checkForChange();
+    //         }
+    //         // otherwise exit and say that no change was detected
+    //         else {
+    //             response.success("No change was detected");
+    //         }
+    //
+    //
+    //
+    //     }, function(e) {
+    //         // handle error
+    //
+    //     });
+    //
+    // }
 
-            var currentTime = new Date().getTime();
-            
-            // if feed var was set, respond with true
-            if(remoteFeedObject.get("shouldFeed")) {
-                response.success("Should feed");
-            }
-            // if we havent reached the time limit, check again    
-            else if (currentTime - startTime < timeLimit) { 
-                
-                checkForChange();
-            }
-            // otherwise exit and say that no change was detected    
-            else {
-                response.success("No change was detected");
-            }
-            
-            
-
-        }, function(e) {
-            // handle error
-        })
-
-    }
+    response.success("test");
 
     // Start the repeat function
     checkForChange();
