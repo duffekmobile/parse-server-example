bac86b9b9b243a9dbda0f50b621b203734171ee4
DavidDuffek <david@duffekmobile.com>
2016-07-27 13:23:34 -0400

    Long poll function

--- cloud/main.js
+++ cloud/main.js
@@ -6,14 +6,44 @@ Parse.Cloud.define('hello', function(req, res) {
 
 
 
-Parse.Cloud.define('checkFeedStatus', function(req, res) {
-    //res.success(
-        var query = new Parse.Query(RemoteFeed);
-        query.first().then(function(object){
-            console.log("Hello! " + object.get("shouldFeed"));
-        },
-        function(error){
-            console.log("Error!");
-        });
-    //);
+Parse.Cloud.define('checkFeedStatus', function(request, response) {
+
+    var startTime = new Date().getTime(); // Get the start time
+    var timeLimit = 10000; // max time 10 seconds
+    
+
+    // Function we want to repeat
+    function checkForChange() {
+        
+        var q = new Parse.Query( Parse.Object.extend('RemoteFeed') );
+        q.get('').then(function(remoteFeedObject) {
+
+            var currentTime = new Date().getTime();
+            
+            // if feed var was set, respond with true
+            if(remoteFeedObject.get("shouldFeed")) {
+                response.success("Should feed");
+            }
+            // if we havent reached the time limit, check again    
+            else if (currentTime - startTime < timeLimit) { 
+                
+                checkForChange();
+            }
+            // otherwise exit and say that no change was detected    
+            else {
+                response.success("No change was detected");
+            }
+            
+            
+
+        }, function(e) {
+            // handle error
+        })
+
+    }
+
+    // Start the repeat function
+    checkForChange();
+
+    
 });
