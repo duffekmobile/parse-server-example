a2613fa386813de2d26cde31f91d386d553cea7d
DavidDuffek <david@duffekmobile.com>
2016-07-27 13:51:53 -0400

    test2

--- cloud/main.js
+++ cloud/main.js
@@ -13,37 +13,38 @@ Parse.Cloud.define('checkFeedStatus', function(request, response) {
     
 
     // Function we want to repeat
-    // function checkForChange() {
-    //
-    //     var q = new Parse.Query( Parse.Object.extend('RemoteFeed') );
-    //     q.get('OhHL5g0ToP').then(function(remoteFeedObject) {
-    //
-    //         var currentTime = new Date().getTime();
-    //
-    //         // if feed var was set, respond with true
-    //         if(remoteFeedObject.get("shouldFeed")) {
-    //             response.success("Should feed");
-    //         }
-    //         // if we havent reached the time limit, check again
-    //         else if (currentTime - startTime < timeLimit) {
-    //
-    //             checkForChange();
-    //         }
-    //         // otherwise exit and say that no change was detected
-    //         else {
-    //             response.success("No change was detected");
-    //         }
-    //
-    //
-    //
-    //     }, function(e) {
-    //         // handle error
-    //
-    //     });
-    //
-    // }
+    function checkForChange() {
 
-    response.success("test");
+
+        var q = new Parse.Query( Parse.Object.extend('RemoteFeed') );
+        q.first().then(function(remoteFeedObject) {
+
+            var currentTime = new Date().getTime();
+
+            // if feed var was set, respond with true
+            if(remoteFeedObject.get("shouldFeed")) {
+                response.success("Should feed");
+            }
+            // if we havent reached the time limit, check again
+            else if (currentTime - startTime < timeLimit) {
+    
+                checkForChange();
+            }
+            // otherwise exit and say that no change was detected
+            else {
+                response.success("No change was detected");
+            }
+
+
+
+        }, function(e) {
+            // handle error
+
+        });
+
+    }
+
+   //response.success("test");
 
     // Start the repeat function
     checkForChange();
